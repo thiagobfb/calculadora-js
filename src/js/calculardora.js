6697860cc0d25b2578f2fa878c3142ff386ff160
C1297675 Thiago Bernardo Fernandes Barbosa <c1297675@interno.bb.com.br>
2020-07-03 11:55:50 -0300

    Criação do objeto Calculadora

---
+++ src/js/calculardora.js
@@ -0,0 +1,19 @@
+let Calculadora = {
+    adicionar: (num1, num2) => {
+        return 0;
+    },
+    subtrair: (num1, num2) => {
+        return 0;
+    },
+    dividir: (num1, num2) => {
+        return 0;
+    },
+    multiplicar: (num1, num2) => {
+        return 0;
+    }
+};
+
+if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
+    module.exports = Calculadora;
+    
+}
