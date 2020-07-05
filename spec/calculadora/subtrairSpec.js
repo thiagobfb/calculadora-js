77075b649463f3bbd6bdab8c0f00bd84866543a5
Thiago Barbosa <thiago.barbosa@nitryx.com>
2020-07-05 17:12:28 -0300

    Implementação dos testes de subtração

--- spec/calculadora/subtrairSpec.js
+++ spec/calculadora/subtrairSpec.js
@@ -2,23 +2,24 @@ describe('Suíte de Testes da Subtração', () => {
    
     let Calculadora = require('../../src/js/calculadora.js');
 
-    it('deve retornar 1 para 3 e 2', () => {
-        expect(Calculadora.subtrair(3,2)).toEqual(1);
+    it('deve retornar 5 para 8 e 3', () => {
+        expect(Calculadora.subtrair(8,3)).toEqual(5);
     });
 
-    xit('deve retornar 6 para 9 e -3 no formato texto', () => {
-        expect(Calculadora.adicionar('9','-3')).toEqual(6);
+    it('deve retornar 5 para 8 e 3 no formato texto', () => {
+        expect(Calculadora.subtrair('8','3')).toEqual(5);
     });
 
-    xit('deve retornar 4.5 para 1.5 e 3', () => {
-        expect(Calculadora.adicionar(1.5, 3)).toEqual(4.5);
+    it('deve retornar 4 para 5.5 e 1.5', () => {
+        expect(Calculadora.subtrair(5.5, 1.5)).toEqual(4);
+        expect(Calculadora.subtrair('5.5', '1.5')).toEqual(4);
     });
 
-    xit('deve retornar 0 quando valor 1 não for numérico', () => {
-        expect(Calculadora.adicionar(undefined, 3)).toEqual(0);
+    it('deve retornar 0 quando valor 1 não for numérico', () => {
+        expect(Calculadora.subtrair(undefined, 3)).toEqual(0);
     });
 
-    xit('deve retornar 0 quando valor 2 não for numérico', () => {
-        expect(Calculadora.adicionar(3, undefined)).toEqual(0);
+    it('deve retornar 0 quando valor 2 não for numérico', () => {
+        expect(Calculadora.subtrair(3, undefined)).toEqual(0);
     });
 });
--- src/js/calculadora.js
+++ src/js/calculadora.js
@@ -8,7 +8,10 @@ let Calculadora = {
         return num1 + num2;
     },
     subtrair: (num1, num2) => {
-        return 0;
+        if (isNaN(num1) || isNaN(num2)) {
+            return 0;
+        }
+        return num1 - num2;
     },
     dividir: (num1, num2) => {
         return 0;
